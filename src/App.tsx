0c147ca3d46672a38bcdad44ad87c4b19bb88417
Gallegos19 <223204@ids.upchiapas.edu.mx>
2024-03-03 20:33:29 -0600

    Front sencillo completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {io} from 'socket.io-client'; // Importa el módulo io de socket.io-client
 import './App.css';
 
-function App() {
+const OrderForm: React.FC = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    price: 0,
+  });
+
+  useEffect(() => {
+    const socket = io("http://35.174.81.13:4000");
+    socket.on("receiveData", (orden: any) => { // Asegúrate de tipar correctamente tus datos según tu estructura de datos
+      console.log(orden);
+      alert("Pago realizado: " + JSON.stringify(orden));
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://54.89.191.56:3000/Api/orden', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log('Order submitted successfully!');
+        // Limpiar los campos después de enviar
+        setFormData({
+          name: '',
+          price: 0,
+        });
+      } else {
+        console.error('Failed to submit order');
+      }
+    } catch (error) {
+      console.error('Error submitting order:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="order-form">
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="price">Price:</label>
+      <input
+        type="number"
+        id="price"
+        name="price"
+        value={formData.price}
+        onChange={handleChange}
+      />
+
+      <button onClick={handleSubmit}>Submit Order</button>
     </div>
   );
-}
+};
 
-export default App;
+export default OrderForm;
